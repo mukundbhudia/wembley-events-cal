040b14a4c798d4a9a571180f123f1fc546e1cf29
Mukund Bhudia <mukund.bhudia@gmail.com>
2017-02-25 19:16:20 +0000

    Formed scraped events into iCal format for file saving

--- src/app.js
+++ src/app.js
@@ -1,9 +1,11 @@
 var Xray = require('x-ray');
 var xray = Xray();
+var ical = require('ical-generator'),
+    http = require('http');
 
-var URL = "https://www.brent.gov.uk/events-and-whats-on-calendar?eventCat=Wembley%20Stadium%20events";
+var sourceUrl = "https://www.brent.gov.uk/events-and-whats-on-calendar?eventCat=Wembley%20Stadium%20events";
 
-xray(URL, {
+xray(sourceUrl, {
     pageTitle: 'title',
         events: xray('div.brent_newEvent', [{
         title: 'a.heading',
@@ -11,10 +13,34 @@ xray(URL, {
         longDescription: 'div.brent_newEventRight',
     }])
 })(function(err, content) {
-    for (var i=0; i < content.events.length; i++) {
-        var eDateString = content.events[i].description.split(',');
-        var eDate = new Date(eDateString[0]);
-        console.log(content.events[i].title + ' < on > ' + eDate);
-        console.log(content.events[i].longDescription);
+    if (content) {
+        var events = [];
+        for (var i=0; i < content.events.length; i++) {
+            var eDateString = content.events[i].description.split(',');
+            var eDate = new Date(eDateString[0]);
+            var event = {
+                start: eDate,
+                allDay: true,
+                summary: content.events[i].title,
+                description: content.events[i].longDescription,
+                location: "Wembley Stadium"
+            };
+            events.push(event);
+        }
+        console.log(events.length + " events found");
+        var cal = ical({
+            name: 'Wembley Stadium Events',
+            timezone: 'Europe/London',
+            events: events
+        });
+        cal.save('wemb-cal.ics', function(err, doc) {
+            if (err) {
+                console.error(err);
+            } else {
+                console.log("Calendar saved");
+            }
+        });
+    } else if (err) {
+        console.error(err);
     }
 });
