742e09d1d323e33f66d2ec78c29eacba82700049
Yousef Alam <yousef.alam@ticketmaster.co.uk>
2019-06-06 15:18:22 +0100

    Add arena cal, dont use http

--- src/app.js
+++ src/app.js
@@ -1,9 +1,6 @@
 var Xray = require('x-ray');
 var xray = Xray();
-var ical = require('ical-generator'),
-    http = require('http');
-
-var port = process.env.PORT || 5000;
+var ical = require('ical-generator');
 
 var sourceUrl = "https://www.brent.gov.uk/events-and-whats-on-calendar?eventCat=Wembley%20Stadium%20events";
 
@@ -11,13 +8,13 @@ xray(sourceUrl, {
     pageTitle: 'title',
         events: xray('div.brent_newEvent', [{
         title: 'a.heading',
-        description: 'div.brent_newEventTimes',
+        description: 'div.brent_newEventDetails',
         longDescription: 'div.brent_newEventRight',
     }])
 })(function(err, content) {
     if (content) {
         var events = [];
-        for (var i=0; i < content.events.length; i++) {
+        for (var i = 0; i < content.events.length; i++) {
             var eDateString = content.events[i].description.split(',');
             var eDate = new Date(eDateString[0]);
             var event = {
@@ -28,6 +25,7 @@ xray(sourceUrl, {
                 location: "Wembley Stadium"
             };
             events.push(event);
+            console.log(event);
         }
         console.log(events.length + " events found");
         var cal = ical({
@@ -37,16 +35,57 @@ xray(sourceUrl, {
             description: "Wembley Stadium events calendar",
             events: events
         });
-        http.createServer(function(req, res) {
-            cal.serve(res);
-        }).listen(port, function() {
-            console.log('Server running at http://127.0.0.1:' + port + '/');
-        });
         cal.save('wemb-cal.ics', function(err, doc) {
             if (err) {
                 console.error(err);
             } else {
-                console.log("Calendar saved");
+                console.log("Stadium calendar saved");
+            }
+        });
+    } else if (err) {
+        console.error(err);
+    }
+});
+
+var sourceUrl = "https://www.ssearena.co.uk/events";
+
+xray(sourceUrl, {
+    pageTitle: 'title',
+        events: xray('div.entry', [{
+        title: 'h3',
+        date: 'div.date',
+        description: 'Not Available',
+        longDescription: 'div.details',
+    }])
+})(function(err, content) {
+    if (content) {
+        var events = [];
+        for (var i = 0; i < content.events.length; i++) {
+            var eDateString = content.events[i].date.trim();
+            var eDate = new Date(eDateString);
+            var event = {
+                start: eDate,
+                allDay: true,
+                summary: content.events[i].title.trim(),
+                description: content.events[i].longDescription.trim().split('…')[0] + '...',
+                location: "SSE Arena Wembley"
+            };
+            events.push(event);
+            console.log(event)
+        }
+        console.log(events.length + " events found");
+        var cal = ical({
+            name: 'Wembley Stadium Events',
+            timezone: 'Europe/London',
+            location: "Wembley Stadium",
+            description: "Wembley Stadium events calendar",
+            events: events
+        });
+        cal.save('arena-cal.ics', function(err, doc) {
+            if (err) {
+                console.error(err);
+            } else {
+                console.log("Arena calendar saved");
             }
         });
     } else if (err) {
